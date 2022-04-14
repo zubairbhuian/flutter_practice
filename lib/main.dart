aeeddc92ee2ed9773d8d8648d5ce8bdf1f8ec5d7
“[Zubair <[zubairbhuian@gmail.com]>
2022-04-14 15:41:10 +0600

    14.8

--- lib/main.dart
+++ lib/main.dart
@@ -30,6 +30,19 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+// !Formate Time
+  String formatTime(Duration duration) {
+    String twoDigits(int n) => n.toString().padLeft(2, '0');
+    final hours = twoDigits(duration.inHours);
+    final minutes = twoDigits(duration.inMinutes.remainder(60));
+    final seconds = twoDigits(duration.inSeconds.remainder(60));
+    return [
+      if (duration.inHours > 0) hours,
+      minutes,
+      seconds,
+    ].join(':');
+  }
+
   final audioPlayer = AudioPlayer();
   bool isPlaying = false;
   Duration duration = Duration.zero;
@@ -53,6 +66,36 @@ class _MyHomePageState extends State<MyHomePage> {
             width: double.infinity,
             height: 350,
             fit: BoxFit.cover,
+          ),
+        ),
+        const SizedBox(
+          height: 24,
+          child: Text(
+            'The Flutter Song',
+            style: TextStyle(fontSize: 22, fontWeight: FontWeight.w600),
+          ),
+        ),
+        const SizedBox(
+          height: 24,
+          child: Text(
+            'Zubair Bhuian',
+            style: TextStyle(fontSize: 16, fontWeight: FontWeight.w400),
+          ),
+        ),
+        Slider(
+          min: 0,
+          max: duration.inSeconds.toDouble(),
+          value: position.inSeconds.toDouble(),
+          onChanged: (value) async {},
+        ),
+        Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text(formatTime(position)),
+              Text(formatTime(duration - position)),
+            ],
           ),
         )
       ],
