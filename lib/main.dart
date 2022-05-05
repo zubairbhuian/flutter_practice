682db63454c1be27e2aebb2b1ff6144fc6bdc14c
“[Zubair <[zubairbhuian@gmail.com]>
2022-05-05 09:31:10 +0600

    5.2

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_practice/home_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,51 +12,13 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
         // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(),
+      home: const HomeScreen(),
     );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key}) : super(key: key);
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  @override
-  Widget build(BuildContext context) {
-    final data = [
-      "this is one",
-      "this is two",
-      "this is three",
-      "this is four",
-      "this is five",
-      "this is six",
-      "this is seven",
-      "this is eight",
-      "this is nine",
-      "this is ten",
-      "this is eleven",
-    ];
-    return Scaffold(
-        appBar: AppBar(
-          title: const Text('Grid View'),
-        ),
-        body: ListView.builder(
-            itemCount: data.length,
-            itemBuilder: ((context, index) {
-              return Container(
-                color: Colors.red,
-                height: 100,
-                child: Text('$data[index]'),
-              );
-            })));
   }
 }
