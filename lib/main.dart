b481d8ff568a239179347794782ff0bc879a4f98
Zubair <74538410+zubairbhuian@users.noreply.github.com>
2022-07-08 10:40:35 +0600

    12323

--- lib/main.dart
+++ lib/main.dart
@@ -11,6 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
         // is not restarted.
@@ -31,31 +32,10 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
-    double screenWith = MediaQuery.of(context).size.width;
-    double screenHight = MediaQuery.of(context).size.height;
-
     return Scaffold(
         body: Container(
       color: Colors.blue,
       height: double.maxFinite,
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Flexible(
-            child: Container(
-              width: double.maxFinite,
-              height: 700,
-              color: Colors.red,
-              child: Column(
-                children: [
-                  Text("$screenWith, $screenHight "),
-                  const Text('Demo One'),
-                ],
-              ),
-            )
-          ),
-        ],
-      ),
     ));
   }
 }
