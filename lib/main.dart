198886d818e16f0fb3e2c43d3469f741ddc293d7
Zubair <74538410+zubairbhuian@users.noreply.github.com>
2022-07-08 10:59:58 +0600

    3123

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_rating_bar/flutter_rating_bar.dart';
 
 void main() {
   runApp(const MyApp());
@@ -36,6 +37,24 @@ class _MyHomePageState extends State<MyHomePage> {
         body: Container(
       color: Colors.blue,
       height: double.maxFinite,
+      child: Center(
+        child: RatingBar.builder(
+          initialRating: 3,
+          minRating: 1,
+          direction: Axis.horizontal,
+          allowHalfRating: true,
+          itemCount: 5,
+         
+          itemPadding: const EdgeInsets.symmetric(horizontal: 4.0),
+          itemBuilder: (context, _) => const Icon(
+            Icons.star,
+            color: Colors.amber,
+          ),
+          onRatingUpdate: (rating) {
+            print(rating);
+          },
+        ),
+      ),
     ));
   }
 }
